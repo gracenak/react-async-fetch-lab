847299db8cdbc40db6164de338e3fb74ecb92f33
gracenak <gracenak@gmail.com>
2021-03-25 18:13:22 -0700

    commit tree 2c6bb6376ee09137144cfbf38c2107bd8b673a3c

--- src/App.js
+++ src/App.js
@@ -1,1 +1,30 @@
 // create your App component here
+import React, { Component } from 'react'
+
+class App extends Component {
+
+  state = {
+      peopleInSpace: []
+  }
+
+  componentDidMount() {
+    fetch('http://api.open-notify.org/astros.json')
+      .then(response => response.json())
+      .then(({people}) => this.setState({ peopleInSpace: people }))
+
+  }
+
+  render() {
+    return (
+      <div>
+        {this.state.peopleInSpace.map((person, id) => 
+        <h3 key={id}>Name: {person.name}, Craft: {person.craft}</h3>)}
+      </div>
+    )
+  }
+
+
+
+}
+
+export default App
